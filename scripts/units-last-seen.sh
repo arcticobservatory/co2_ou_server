70f3226aa76aac78a55aa2dd7ff958359208161a
Mike Murphy <michael.j.murphy@uit.no>
2019-08-02 08:04:58 +0200

    Reformat date output

--- scripts/units-last-seen.sh
+++ scripts/units-last-seen.sh
@@ -3,8 +3,10 @@
 pingsfile=$(find var/pings/* | sort | tail -n2)
 # For each unique hardware id in the pings file
 for hwid in $(cut -f2 $pingsfile | sort | uniq); do
-	# Search backwards through the pings file for the last ping,
-	# then re-order the output fields (site code, time, hardware id)
+	# tac --- Search backwards through the pings file for the last ping,
+	# awk --- Re-order the output fields (site code, time, hardware id)
+	# sed --- Reformat the ISO date to be more readable
 	tac $pingsfile | grep $hwid | head -n1 \
-		| awk 'BEGIN {OFS="\t"}; {print $3, $1, $2}' 
+		| awk 'BEGIN {OFS="\t"}; {print $3, $1, $2}' \
+		| sed -E 's/([0-9-]{10})T([0-9:]{8}).[0-9]+/\1 \2/'
 done | sort
