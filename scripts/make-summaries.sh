b4a7c922c88ebee556d290246df3c0b0a58ee501
mmu019 <mmu019@mmu019-1.cs.uit.no>
2020-07-11 03:30:24 +0200

    Pass additional args to make

--- scripts/make-summaries.sh
+++ scripts/make-summaries.sh
@@ -6,4 +6,4 @@ echo 2>&1 "Refreshing web summaries at $(date -Iseconds)"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Run Make
-make -C "$DIR/../database/" DATA_DIR=../remote_data/ DB_DIR=../var WEB_PUB_DIR=../var/pub/ web
+make "$@" -C "$DIR/../database/" DATA_DIR=../remote_data/ DB_DIR=../var WEB_PUB_DIR=../var/pub/ web
